311f6b294627e9e1acd76bcb9e1e8ab7d5e43ac5
Damon Ciarelli <damon@mill.co.uk>
2018-01-31 18:07:39 -0500

    record .wav files

--- main.py
+++ main.py
@@ -6,29 +6,28 @@ from flask import render_template
 from flask import request
 from flask import session
 from werkzeug.utils import secure_filename
+import os
 
-from google.cloud import storage
 
 import os
 import uuid
 
 app = Flask(__name__)
 
-# Configure this environment variable via app.yaml
-CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
-# [end config]
-
 @app.route("/")
 def welcome():
+    print ("ok")
     session_id = request.cookies.get('session_id')
     if session_id:
         all_done = request.cookies.get('all_done')
         if all_done:
-            return render_template("thanks.html")
+            response = make_response(redirect('/start'))
+            response.set_cookie('session_id', '', expires=0)
+            return response
         else:
             return render_template("record.html")
     else:
-        return render_template("welcome.html")
+        return make_response(redirect('/start'))
 
 @app.route("/legal")
 def legal():
@@ -39,6 +38,7 @@ def start():
     response = make_response(redirect('/'))
     session_id = uuid.uuid4().hex
     response.set_cookie('session_id', session_id)
+    response.set_cookie('all_done', '', expires=0)
     return response
 
 @app.route('/upload', methods=['POST'])
@@ -48,18 +48,15 @@ def upload():
         make_response('No session', 400)
     word = request.args.get('word')
     audio_data = request.data
-    filename = word + '_' + session_id + '_' + uuid.uuid4().hex + '.ogg'
-    secure_name = secure_filename(filename)
+    filename = word + '----' + uuid.uuid4().hex + '.wav'
+    secure_name = filename
     # Left in for debugging purposes. If you comment this back in, the data
     # will be saved to the local file system.
-    #with open(secure_name, 'wb') as f:
-    #    f.write(audio_data)
+    with open(os.path.join('recordings', secure_name), 'wb') as f:
+       f.write(audio_data)
     # Create a Cloud Storage client.
-    gcs = storage.Client()
-    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-    blob = bucket.blob(secure_name)
-    blob.upload_from_string(audio_data, content_type='audio/ogg')
-    return make_response('All good')
+    response = make_response('All good')
+    return response
 
 # CSRF protection, see http://flask.pocoo.org/snippets/3/.
 @app.before_request
@@ -76,7 +73,7 @@ def generate_csrf_token():
 
 app.jinja_env.globals['csrf_token'] = generate_csrf_token
 # Change this to your own number before you deploy.
-app.secret_key = os.environ['SESSION_SECRET_KEY']
+app.secret_key = "bibblebabbleblubberdooodle"
 
 if __name__ == "__main__":
     app.run(debug=True)
